ce726aad9eb3692342fb402b9d152572cf215204
SERGIO SALANITRI <ssalanitri@user.noreply.github.com>
2025-01-13 20:27:07 -0300

    #32 Allow resign

--- chess_game/static/js/chessExtensions.js
+++ chess_game/static/js/chessExtensions.js
@@ -1,22 +1,22 @@
-const Chess = require('chess.js');
+// import Chess, { prototype } from 'chess.js';
 
-// Add a custom method to the Chess prototype
-Chess.prototype.isPawnPromotionPossible = function (color) {
-    const rank = color === 'w' ? '7' : '2';
-    const board = this.board();
+// // Add a custom method to the Chess prototype
+// prototype.isPawnPromotionPossible = function (color) {
+//     const rank = color === 'w' ? '7' : '2';
+//     const board = this.board();
 
-    for (let file = 0; file < 8; file++) {
-        const square = board[rank][file];
-        if (square && square.type === 'p' && square.color === color) {
-            return true; // Pawn is ready for promotion
-        }
-    }
-    return false;
-};
+//     for (let file = 0; file < 8; file++) {
+//         const square = board[rank][file];
+//         if (square && square.type === 'p' && square.color === color) {
+//             return true; // Pawn is ready for promotion
+//         }
+//     }
+//     return false;
+// };
 
-// Add a custom method to the Chess prototype
-Chess.prototype.isGameOver = function () {
-    return this.game_over();
-};  
+// // Add a custom method to the Chess prototype
+// prototype.isGameOver = function () {
+//     return this.game_over();
+// };  
 
-module.exports = Chess;
+// export default Chess;
